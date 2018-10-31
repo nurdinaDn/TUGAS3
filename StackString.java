d58641912e3e3f13a8f0fbc6d16142f493d263cb
Nurdina <43734373+nurdinaDn@users.noreply.github.com>
2018-10-31 20:38:21 +0800

    Create StackString.java

---
+++ StackString.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class StackString {
+	ArrayList<String> kelas= new ArrayList<String>();
+	int top = -1;
+	
+	public void push(String value){
+		top = top + 1;
+		kelas.add(top, value);
+	}
+	
+	public String pop(){
+		String value = "TIDAK ADA MAHASISWA";
+		if(!kelas.isEmpty()){
+			value = kelas.get(top);
+			kelas.remove(top);
+			top = top - 1;
+			
+		}
+		return value;
+	}
+	
+	public void cetak(){
+		System.out.println("TOP :" +top+" # KELAS: "+kelas.toString());
+	}
+}
