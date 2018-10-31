c8631a1616af85d13e39fc43c4c70bde136052c0
Nurdina <43734373+nurdinaDn@users.noreply.github.com>
2018-10-31 23:53:23 +0800

    Create mahasiswa.java

---
+++ mahasiswa.java
@@ -0,0 +1,20 @@
+
+
+public class mahasiswa {
+	String nama;
+	String nim;
+	char kelas;
+	String alamat;
+	void printNama() {
+		System.out.println(nama);
+	}
+	void printNim() {
+		System.out.println(nim);
+	}
+	void printkelas() {
+		System.out.println(kelas);
+	}
+	void printAlamat() {
+		System.out.println(alamat);
+	}
+}
