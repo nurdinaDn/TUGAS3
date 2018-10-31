61d5631f77b01f4e58d90815bc3c84eb98c5c7a6
Nurdina <43734373+nurdinaDn@users.noreply.github.com>
2018-10-31 22:20:55 +0800

    Create MainQueueFloat.java

---
+++ MainQueueFloat.java
@@ -0,0 +1,33 @@
+public class MainQueueFloat {
+
+	public static void main(String[] args) {
+		QueueFloat angka = new QueueFloat();
+		angka.cetak();
+		
+		angka.insert(3.905f);
+		angka.cetak();
+		
+		angka.insert(4.7f);
+		angka.cetak();
+		
+		angka.insert(5.6f);
+		angka.cetak(); 
+		
+		Float o1 = angka.get();
+		System.out.println(o1);
+		angka.cetak();
+		
+		Float o2 = angka.get();
+		System.out.println(o2);
+		angka.cetak();
+		
+		Float o3 = angka.get();
+		System.out.println(o3);
+		angka.cetak();
+		
+		Float o4 = angka.get();
+		System.out.println(o4);
+		angka.cetak();
+	}
+
+}
