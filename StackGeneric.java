db3ec8ea425ac80a265fa4a6b61fcc2b807cb7c4
Nurdina <43734373+nurdinaDn@users.noreply.github.com>
2018-11-01 01:23:35 +0800

    Create StackGeneric.java

---
+++ StackGeneric.java
@@ -0,0 +1,10 @@
+public class StackGeneric<T> {
+	public T Mahasiswa;
+	protected void setMahasiswa(T Mahasiswa){
+		this.Mahasiswa = Mahasiswa;
+	}
+	protected T getMahasiswa(){
+		return Mahasiswa;
+	}
+
+}
